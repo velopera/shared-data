515aa888d39d351b3c3fe160f6e37d8b319de4d8
Simon Vogl <simon+velopera@velopera.voxel.at>
2024-03-25 11:31:37 +0100

    Initial commit

---
+++ dist/index.d.ts
@@ -0,0 +1,1 @@
+export * from "./velopera-data";
---
+++ dist/velopera-data.d.ts
@@ -0,0 +1,26 @@
+export interface ParsedLoginData {
+    networkStatus?: string;
+    rsrp?: number;
+    mcc?: number;
+    mnc?: number;
+    cid?: number;
+    band?: number;
+    areaCode?: number;
+    op?: number;
+    modem?: string;
+    fw?: string;
+    iccid?: string;
+}
+export interface LoginMsg {
+    status?: string;
+    rsrp?: number;
+    mcc?: string;
+    mnc?: string;
+    cid?: string;
+    band?: string;
+    areaCode?: string;
+    op?: string;
+    modem?: string;
+    fw?: string;
+    iccid?: string;
+}
